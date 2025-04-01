3fa90377d5d7aea73ae6ba4de20fd6aa3c77da3b
Regan <reganbc12@gmail.com>
2025-03-31 19:06:46 -0700

    Removed some comments that were redundant

--- app/src/main/java/de/kai_morich/simple_bluetooth_terminal/TextUtil.java
+++ app/src/main/java/de/kai_morich/simple_bluetooth_terminal/TextUtil.java
@@ -69,9 +69,6 @@ final class TextUtil {
         }
     }
 
-    /**
-     * use https://en.wikipedia.org/wiki/Caret_notation to avoid invisible control characters
-     */
     static CharSequence toCaretString(CharSequence s, boolean keepNewline) {
         return toCaretString(s, keepNewline, s.length());
     }
